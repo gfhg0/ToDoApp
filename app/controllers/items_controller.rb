903ea2f957d9351cb0a427908a879c73db7f9937
gfhg0 <grzkwasniewski@gmail.com>
2016-06-07 01:53:02 +0200

    Haml

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 	before_action :set_list
+	before_action :set_item, except: [:create]
 
 	def create
 		@item = @list.items.create(item_params)
@@ -7,10 +8,25 @@ class ItemsController < ApplicationController
 	end
 
 
+	def destroy
+    	@item.destroy
+    	redirect_to @list
+  	end
+
+  	def complete
+  		@item.update_attribute(:completed_at, Time.now)
+		redirect_to @list, notice: "Completed"
+  	end
+
+
 	private
 
 	def set_list
 		@list = List.find(params[:list_id])
+	end
+
+	def set_item
+		@item = @list.items.find(params[:id])
 	end
 
 	def item_params
